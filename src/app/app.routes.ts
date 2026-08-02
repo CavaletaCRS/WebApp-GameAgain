b73797c8fb97709275b99270f74f1214c7fd2dbf
CavaletaCRS <christiansubelet@gmail.com>
2026-08-02 15:30:00 +0200

    Creazione componenti organizzati nelle cartelle
    Creazione componenti per Pagine(Categorie/Home/Not Found), services e componenti condivisi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Game Again - Videogiochi usati',
+    loadComponent: () =>
+      import('./pages/home/home').then((component) => component.Home),
+  },
+  {
+    path: 'categoria/:category',
+    title: 'Categoria - Game Again',
+    loadComponent: () =>
+      import('./pages/category/category').then(
+        (component) => component.Category,
+      ),
+  },
+  {
+    path: '**',
+    title: 'Pagina non trovata - Game Again',
+    loadComponent: () =>
+      import('./pages/not-found/not-found').then(
+        (component) => component.NotFound,
+      ),
+  },
+];
